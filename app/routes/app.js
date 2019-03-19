b73279499a51839c04de6fe78203d2f62e5c7817
go <eiringonzales@gmail.com>
2019-03-19 12:21:30 +0800

    Added catch all

--- app/routes/app.js
+++ app/routes/app.js
@@ -276,7 +276,7 @@ module.exports = function(app, passport) {
     }
   });
 
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
   });
 
